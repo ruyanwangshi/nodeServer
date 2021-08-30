7834861b93daf00a511a5c69d9d52d14d7085af0
liudong <liudong-ls@bestpay.com.cn>
2021-08-30 18:07:20 +0800

    task: 推

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const path = require('path')
 const koa = require('koa')
+const logger = require('koa-logger')
 const koaBody = require('koa-body')
-const koaStatic = require('koa-static')
+const server = require('koa-static')
 const cors = require('koa-cors')
 const Router = require('./router')
 
 const app = new koa()
-app.use(koaStatic(`${__dirname}`, './public'))
+app.use(logger())
 app.use(cors())
 app.use(koaBody())
+app.use(server(path.join(__dirname, '/public')))
 app.use(Router.routes())
+app.use(Router.allowedMethods())
 
 app.listen(3000, () => {
   console.log('192.168.31.12:3000')
